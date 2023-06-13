6c6e3e609103fb58bc1d21d408de999b1108818e
NickP007 <nickp@email.ua>
2023-06-13 15:57:46 +0300

    les08 task02: Terraform GKE + Flux + SOPS
    Signed-off-by: NickP007 <nickp@email.ua>

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-### Тиждень 7
+## Тиждень 8
+
+### Задача 2
+
+1. Отримаємо інфраструктурний код для розгортання за допомогою **terraform**:
+
+  ```bash
+  git clone --branch tf_gke_flux_sops --single-branch https://github.com/NickP007/les07.git tf_sops
+  cd tf_sops
+  ```
+
+2. Імпортуємо персональний токен GitHub
+
+  ```bash
+  export TF_VAR_GITHUB_OWNER=${YOUR_GITHUB_ACCOUNT}
+  export TF_VAR_GITHUB_TOKEN=${YOUR_GITHUB_ACCESS_TOKEN}
+  ```
+
+3. Створемо Google Storage Bucket `tf-gke-demo`
+
+4. Розгорнемо Kubernetes Cluster з встановленою системою **flux** за допомогою **terraform**:
+
+  ```bash
+  terraform init
+  terraform apply
+  ```
--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "GOOGLE_PROJECT" {
   type        = string
-  description = "GCP project name"
+  description = "GCP project to use"
 }
 
 variable "GOOGLE_REGION" {
@@ -9,26 +9,30 @@ variable "GOOGLE_REGION" {
   description = "GCP region to use"
 }
 
-variable "GKE_MACHINE_TYPE" {
-  type        = string
-  default     = "g1-small"
-  description = "Machine type"
-}
-
 variable "GKE_NUM_NODES" {
   type        = number
   default     = 2
   description = "GKE nodes number"
 }
 
-variable "GKE_CLUSTER_NAME" {
+variable "GITHUB_OWNER" {
   type        = string
-  default     = "main"
-  description = "GKE cluster name"
+  description = "GitHub owner repository to use"
 }
 
-variable "GKE_POOL_NAME" {
+variable "GITHUB_TOKEN" {
   type        = string
-  default     = "main"
-  description = "GKE pool name"
+  description = "GitHub personal access token"
+}
+
+variable "FLUX_GITHUB_REPO" {
+  type        = string
+  default     = "flux-gitops"
+  description = "Flux GitOps repository"
+}
+
+variable "FLUX_GITHUB_TARGET_PATH" {
+  type        = string
+  default     = "clusters"
+  description = "Flux manifests subdirectory"
 }
